01ef4f6edb20329162ebd1c952f65c6196a1fd5d
JacquesIversen <jackiracki1@gmail.com>
2023-10-18 16:54:33 +0000

    navlink updated with Router

--- src/App.js
+++ src/App.js
@@ -2,28 +2,21 @@ import styles from "./App.module.css";
 import Button from "react-bootstrap/Button";
 import Content from "./pages/Content";
 import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <Content />
-      <h1>Hello World din bitch</h1>
-      <Button variant="primary">Primary</Button>{" "}
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <h1>Ooops, this page does not exist</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { Navbar, Nav } from "react-bootstrap";
-/* import { Link } from "react-router-dom"; */
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"; // Import the CSS file
 import logo from "../Assets/logo.png";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const NavBar = () => {
-  const [isSticky, setSticky] = useState(false);
-
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setSticky(true);
-    } else {
-      setSticky(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <Navbar className={styles.NavBar} expand="lg">
-      <Navbar.Brand to="/">
-        <img src={logo} alt="logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          {/* Add your navigation links here */}
-          <Nav.Link /* as={Link} */ to="/home">
-            <i class="fa-solid fa-house"></i> Home
-          </Nav.Link>
-          <Nav.Link /* as={Link} */ to="/about">
-            <i class="fa-solid fa-right-to-bracket"></i> Sign In
-          </Nav.Link>
-          <Nav.Link /* as={Link} */ to="/about">
-            <i class="fa-solid fa-user-plus"></i> Sign Up
-          </Nav.Link>
-          {/* Add more links as needed */}
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar className={styles.navbar} expand="lg">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-house"></i> Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fa-solid fa-right-to-bracket"></i> Sign In
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
+              <i className="fa-solid fa-user-plus"></i> Sign Up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
