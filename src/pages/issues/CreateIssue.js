ffd8ba5339797b3ac39e9b6825abc12e44465342
JacquesIversen <jackiracki1@gmail.com>
2023-10-25 21:23:48 +0000

    styling to create issue page added

--- src/pages/issues/CreateIssue.js
+++ src/pages/issues/CreateIssue.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import styles from "../../styles/CreateIssue.module.css";
 import Asset from "../../components/Asset";
 import UploadIssue from "../../Assets/TiredAsIAm.png";
-import { Image } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
 import Cookies from "js-cookie";
@@ -71,108 +71,112 @@ function CreateIssue() {
   };
 
   const textFields = (
-    <div className="text-center">
-      <Form.Group>
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          name="title"
-          value={title}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Car</Form.Label>
-        <Form.Control
-          type="text"
-          name="car"
-          value={car}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Model</Form.Label>
-        <Form.Control
-          type="text"
-          name="model"
-          value={model}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Year</Form.Label>
-        <Form.Control
-          type="text"
-          name="year"
-          value={year}
-          placeholder="State the production year of your vehicle."
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Engine Size</Form.Label>
-        <Form.Control
-          type="text"
-          name="engine_size"
-          value={engine_size}
-          placeholder="ex. 2.0"
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={12}
-          name="description"
-          value={description}
-          placeholder="Be as precise when stating your problem, and be sure to include any solutions you might have tried already"
-          onChange={handleChange}
-        />
-      </Form.Group>
+    <div>
+      <div className={styles.formLabel}>Title</div>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={handleChange}
+        className={styles.formInput}
+      />
 
-      <Button onClick={() => history.goBack()}>cancel</Button>
-      <Button type="submit">create</Button>
+      <div className={styles.formLabel}>Car</div>
+      <input
+        type="text"
+        name="car"
+        placeholder="ex. Vauxhall"
+        value={car}
+        onChange={handleChange}
+        className={styles.formInput}
+      />
+
+      <div className={styles.formLabel}>Model</div>
+      <input
+        type="text"
+        name="model"
+        value={model}
+        placeholder="ex. Astra"
+        onChange={handleChange}
+        className={styles.formInput}
+      />
+
+      <div className={styles.formLabel}>Year</div>
+      <input
+        type="text"
+        name="year"
+        value={year}
+        placeholder="Production Year"
+        onChange={handleChange}
+        className={styles.formInput}
+      />
+
+      <div className={styles.formLabel}>Engine Size</div>
+      <input
+        type="text"
+        name="engine_size"
+        value={engine_size}
+        placeholder="ex. 2.0"
+        onChange={handleChange}
+        className={styles.formInput}
+      />
+
+      <div className={styles.formLabel}>Description</div>
+      <textarea
+        rows={12}
+        name="description"
+        value={description}
+        placeholder="Be as precise when stating your problem, and be sure to include any solutions you might have tried already"
+        onChange={handleChange}
+        className={styles.formTextarea}
+      />
+
+      <button onClick={() => history.goBack()} className={styles.cancelButton}>
+        cancel
+      </button>
+      <button type="submit" className={styles.formButton}>
+        create
+      </button>
     </div>
   );
 
   return (
     <Form onSubmit={handleSubmit}>
       <Container
-        className={`${styles.Container} d-flex flex-column justify-content-center`}
+        className={`${styles.formContainer} ${styles.imageUploadSection} `}
       >
-        <Form.Group className="text-center">
-          {image ? (
-            <>
-              <figure>
-                <Image src={image} />
-              </figure>
-              <div>
-                <Form.Label htmlFor="image-upload">Change Image</Form.Label>
-              </div>
-            </>
-          ) : (
-            <Form.Label
-              className="d-flex justify-content-center"
-              htmlFor="image-upload"
-            >
-              <Asset
-                src={UploadIssue}
-                message="Tap the tired man, to upload your own picture"
-              />
-            </Form.Label>
-          )}
+        {image ? (
+          <>
+            <figure>
+              <Image src={image} className={styles.formElement} />
+            </figure>
+            <div>
+              <Form.Label htmlFor="image-upload" className={styles.formElement}>
+                Change Image
+              </Form.Label>
+            </div>
+          </>
+        ) : (
+          <Form.Label
+            htmlFor="image-upload"
+            className={styles.assetUploadSection}
+          >
+            <Asset
+              src={UploadIssue}
+              message="Tap the tired man, to upload your own picture"
+            />
+          </Form.Label>
+        )}
 
-          <Form.File
-            id="image-upload"
-            accept="image/*"
-            onChange={handleChangeImage}
-            ref={imageInput}
-          />
-        </Form.Group>
-        <div className="d-md-none">{textFields}</div>
+        <Form.File
+          id="image-upload"
+          accept="image/*"
+          onChange={handleChangeImage}
+          ref={imageInput}
+          className={styles.formElement}
+        />
+        <div>{textFields}</div>
       </Container>
-      <Container>{textFields}</Container>
     </Form>
   );
 }
